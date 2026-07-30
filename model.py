248a765cf5854c6def66159caccf12a98af6ab86
Anirod310 <bousek.dorian@gmail.com>
2026-07-30 12:35:37 +0200

    Add initial implementation of Rainbow DQN model (only Dueling DQN for the moment)

---
+++ model.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Rainbow(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(Rainbow, self).__init__()
+        self.feature_layer = nn.Sequential(nn.Linear(input_dim, 128),
+                                      nn.ReLU())
+        self.value_stream = nn.Sequential(nn.Linear(128, 128),
+                                     nn.ReLU(),
+                                     nn.Linear(128, 1))
+        self.advantage_stream = nn.Sequential(nn.Linear(128, 128),
+                                         nn.ReLU(),
+                                         nn.Linear(128, output_dim))
+
+    def forward(self, x):
+        features = self.feature_layer(x)
+        v= self.value_stream(features)
+        a = self.advantage_stream(features)
+        q_values = v + (a - torch.mean(a, dim=-1, keepdim=True))
+
+        return q_values
+
+
+        
+        
